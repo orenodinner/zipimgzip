2c1463ab04a0939f7de54d72251bf4ed61eec8a5
orenodinner <orenodinner@gmail.com>
2022-10-07 00:04:11 +0900

    UnzupMemory作成途中

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use std::path::Path;
 
 use std::fs::File;
-use std::io::{Read, BufReader, Seek, SeekFrom, Result};
+use std::io::{Read, BufReader, Seek, SeekFrom, Result, Stdout};
 use std::fs;
 use std::io;
+
+use zip::read::ZipFile;
 
 pub  struct  Input_ZipFile {
 
@@ -16,13 +18,63 @@ pub  struct  Input_ZipFile {
 
 
 impl  Input_ZipFile {
-   pub fn Unzip(&mut self){
-      
+   pub fn Unzip_toMemory(&mut self)->Option<Vec<Stdout>>{
 
-       self.UnzipFile = vec![image::DynamicImage::new_rgb32f(512, 512)];
-       self.debug_str = String::from("change");
-       
+    let fname = std::path::Path::new(&self.InputPath_str);
+         let file = fs::File::open(&fname).unwrap();
+     
+         let mut archive = zip::ZipArchive::new(file).unwrap();
+        let mut MemoryFiles:Vec<Stdout> = Vec::new();
+         for i in 0..archive.len() {
+             let mut file = archive.by_index(i).unwrap();
+             let outpath = match file.enclosed_name() {
+                 Some(path) => path.to_owned(),
+                 None => continue,
+             };
+     
+             {
+                 let comment = file.comment();
+                 if !comment.is_empty() {
+                     println!("File {} comment: {}", i, comment);
+                 }
+             }
+     
+             if (*file.name()).ends_with('/') {
+                 println!("File {} extracted to \"{}\"", i, outpath.display());
+                 fs::create_dir_all(&outpath).unwrap();
+             } else {
+                 println!(
+                     "File {} extracted to \"{}\" ({} bytes)",
+                     i,
+                     outpath.display(),
+                     file.size()
+                 );
+                 if let Some(p) = outpath.parent() {
+                     if !p.exists() {
+                         fs::create_dir_all(&p).unwrap();
+                     }
+                 }
+                 let mut s_out:Stdout = io::stdout();
+                 io::copy(&mut file, &mut s_out).unwrap();
+                MemoryFiles.push(s_out);
+                 let mut outfile = fs::File::create(&outpath).unwrap();
+                 
+             }
+     
+             // Get and Set permissions
+             #[cfg(unix)]
+             {
+                 use std::os::unix::fs::PermissionsExt;
+     
+                 if let Some(mode) = file.unix_mode() {
+                     fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
+                 }
+             }
+         }
+       None
     }
+
+
     pub fn Debug_str(&mut self)-> &String{
         
         return &self.debug_str
@@ -89,6 +141,8 @@ impl  Input_ZipFile {
 
 
    
+
+
 
 
 } 
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,22 @@
 use ResizeImgZiper::Input_ZipFile;
 fn main() {
     println!("Hello, world!");
+    let test_path = String::from("C:\\temp\\test.zip");
+    /* 
     let args: Vec<_> = std::env::args().collect();
     if args.len() < 2 {
         println!("Usage: {} <filename>", args[0]);
         return ;
-    }
+    }*/
     
     
     let mut izip = Input_ZipFile{
         debug_str:String::from("new"),
-        InputPath_str:String::from(&*args[1]),
+       // InputPath_str:String::from(&*args[1]),
+       InputPath_str:String::from(&test_path),
         UnzipFile:vec![image::DynamicImage::new_rgb32f(5, 5)]};
     
-    &izip.Unzip();
+    &izip.Unzip2();
     println!("{:?}",&izip.Debug_str());
     &izip.Debug();
     println!("{:?}",&izip.Debug_str());
